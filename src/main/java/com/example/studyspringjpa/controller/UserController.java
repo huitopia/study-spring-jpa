7e38faf204cae4c9d869b33a9026419e24276d5f
huitopia <huitopia27@gmail.com>
2024-11-06 17:35:35 +0900

    User 생성

---
+++ src/main/java/com/example/studyspringjpa/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.studyspringjpa.controller;
+
+import com.example.studyspringjpa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+}
