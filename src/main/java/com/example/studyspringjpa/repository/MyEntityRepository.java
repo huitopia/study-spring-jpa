2e592d01d2eb26d16eb9cab502a4a003197f236b
huitopia <huitopia27@gmail.com>
2024-11-06 18:10:24 +0900

    JpaRepository

--- src/main/java/com/example/studyspringjpa/repository/MyEntityRepository.java
+++ src/main/java/com/example/studyspringjpa/repository/MyEntityRepository.java
@@ -2,7 +2,19 @@ package com.example.studyspringjpa.repository;
 
 import com.example.studyspringjpa.domain.MyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.swing.*;
+
+@Repository
 public interface MyEntityRepository extends JpaRepository<MyEntity, Long> {
-    /* JPA의 JpaRepository를 상속받아 CRUD 작업을 수행 */
+    /*
+     * JpaRepository
+     *   Spring Data JPA 에서 제공하는 인터페이스
+     *   기본 CRUD, 페이징, 정렬 제공
+     * <MyEntity, Long>
+     *   MyEntity : 엔티티 클래스 이름 지정 (레포가 처리 할 엔티티의 타입)
+     *   Long : 기본 키 타입 지정(@Id)
+     */
+    /* 기본적인 메소드는 작성하지 않고 복잡한 쿼리 필요할 경우 작성함 */
 }
